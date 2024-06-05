097af7a2d83f3edba8765cff1ddbac42d991f7be
arafatmollah <arafatshuvo18@gmail.com>
2024-06-05 13:10:27 +0600

    FileFormat

--- Math.js
+++
@@ -1,2 +0,0 @@
-let got = Math.random();
-console.log(got)
--- Number.js
+++
@@ -1,7 +0,0 @@
-// console.log(1+1);
-let first = '99';
-let second = 10;
-console.log(first+second)
-let third = parseInt(first)
-let final = third+second;
-console.log(parseInt(final));
