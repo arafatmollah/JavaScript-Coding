ad4ea8e4771caeef7bb1c9741f82c15a08a01fb0
arafatmollah <arafatshuvo18@gmail.com>
2024-06-11 23:22:25 +0600

    fibo solution

--- ProblemSolving/Fibonacci.js
+++ ProblemSolving/Fibonacci.js
@@ -10,12 +10,6 @@
     return sequence;
 
     
-    // if (n > 1) {
-    //   while (sequence.length < n) {
-    //       sequence.push(next);
-    //       const previous = sequence[sequence.length - 2];
-    //       next = previous + next;
-    //   }
-    // }
+ 
 
 
