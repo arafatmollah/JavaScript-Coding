23fed3901ffbf3dc431e45bca1284ea5560e29b7
arafatmollah <arafatshuvo18@gmail.com>
2024-06-11 20:20:02 +0600

    some problem solving

---
+++ Basic/LeapYear.js
@@ -0,0 +1,26 @@
+
+let Leapyear = (year)=>{
+    if(year%4===0){
+
+        if(year%400===0){
+
+            if(year%100===0){
+
+                return 'Year is leap year'
+            }
+            else {
+                return 'Year is no leap year'
+            }
+        }
+        else{
+            return 'Not a leap year'
+        }
+    }
+    else{
+        return 'Not a leap year'
+    }
+}
+
+console.log(Leapyear(2000))
+console.log(Leapyear(1999))
+console.log(Leapyear(1990))
